8232f925c5609eabbd27e3986cfb584c4c300f23
empi91 <korpet.filip@gmail.com>
2024-02-03 21:04:54 +0100

    Creating database and tables working

--- database.py
+++ database.py
@@ -13,14 +13,15 @@ PATH = pathlib.Path.cwd() / "data.json"
 
 class Database:
     def __init__(self):
-        self.database = {}
-        # self.load_database()
         self.init_file = "database/database.ini"
         self.config = {}
-        self.load_config()
 
-    def load_config(self):
-        section = "postgresql"
+        self.load_config("postgresql")
+        self.create_database("client_server_db")
+        self.load_config("client_server_db")
+        self.create_tables()
+
+    def load_config(self, section):
         parser = ConfigParser()
         parser.read(self.init_file)
 
@@ -31,34 +32,85 @@ class Database:
         else:
             raise Exception(f"Section {section} not found in the {self.init_file} file")
 
-    def load_database(self):
-        # try:
-        #     if os.path.getsize(PATH) == 0:
-        #         self.database = {}
-        #     else:
-        #         with PATH.open(mode="r", encoding="utf-8") as file:
-        #             self.database = json.load(file)
-        # except FileNotFoundError:
-        #     self.database = {}
-        # except json.decoder.JSONDecodeError as e:
-        #     print(f"Error decoding JSON: {e}")
-        #     print("Problematic JSON text:")
-        #     print(e.doc)
+    def create_database(self, db_name):
         try:
-            print(self.config)
             connection = psycopg2.connect(**self.config)
             connection.autocommit = True
             cursor = connection.cursor()
 
-            query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier("client_server_db"))
+            query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name))
             cursor.execute(query)
 
+            cursor.close()
+            connection.close()
         except psycopg2.errors.DuplicateDatabase as e:
             print(e)
         except (psycopg2.DatabaseError, Exception) as e:
             print(e)
 
+    def create_tables(self):
+        try:
+            with psycopg2.connect(**self.config) as connection:
+                with connection.cursor() as cursor:
+                    queries = (
+                               """
+                               CREATE TABLE users (
+                               user_id SERIAL PRIMARY KEY,
+                               username VARCHAR(50) UNIQUE NOT NULL,
+                               password VARCHAR(50) NOT NULL,
+                               account_type VARCHAR(10) NOT NULL
+                               )
+                               """,
+                               """
+                               CREATE TABLE messages (
+                               message_id SERIAL PRIMARY KEY,
+                               receiver_id INT NOT NULL REFERENCES users(user_id),
+                               sender INT NOT NULL REFERENCES users(user_id),
+                               message VARCHAR(255)
+                               )
+                               """)
 
+                    for query in queries:
+                        cursor.execute(query)
+
+
+        except (psycopg2.DatabaseError, Exception) as e:
+            print(e)
+    def connect_to_db(self):
+
+        pass
+
+    def send_query(self, query):
+        try:
+            with psycopg2.connect(**self.config) as connection:
+                with connection.cursor() as cursor:
+
+                    cursor.execute(query)
+
+
+        except (psycopg2.DatabaseError, Exception) as e:
+            print(e)
+
+    #add user
+    #remove user
+    #send message
+    #check inbox
+    #read inbox
+    #login
+
+    def load_database(self):
+        try:
+            if os.path.getsize(PATH) == 0:
+                self.database = {}
+            else:
+                with PATH.open(mode="r", encoding="utf-8") as file:
+                    self.database = json.load(file)
+        except FileNotFoundError:
+            self.database = {}
+        except json.decoder.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            print("Problematic JSON text:")
+            print(e.doc)
 
     def add_to_database(self, username, user_data):
         self.database[username] = user_data
@@ -74,8 +126,8 @@ class Database:
         self.save_database()
         pass
 
-    # if __name__ == '__main__':
 
 db = Database()
 # db.load_config()
-db.load_database()
+# db.load_database()
+# db.create_database()
