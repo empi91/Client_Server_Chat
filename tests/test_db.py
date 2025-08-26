2e679e7e22998bc02ff0da4bc3a0a879cfdb72d0
empi91 <korpet.filip@gmail.com>
2025-08-26 15:28:51 +0200

    SStarting Database class tests

---
+++ tests/test_db.py
@@ -0,0 +1,31 @@
+""" Test suite for Database and DbHelper classes"""
+
+from db import Database, DbHelper
+from config import config
+
+class TestDatabase(unittest.TestCase):
+    """ Test suite for Database class """
+
+    def SetUp(self):
+        """Setting up for testing Database methods"""
+        
+
+        pass
+
+    
+    def test_database_initialization(self):
+        """Testing if Database class creates an empty JSON database if it doesn't exist.
+        Testing if Database handles connecting to the existing database correctly"""
+
+
+        pass
+
+
+    def test_adding_user_to_db(self):
+        """Testing adding new users with different account types to database. """
+
+
+        pass
+
+
+    
--- tests/test_message.py
+++ tests/test_message.py
@@ -99,3 +99,4 @@ class TestMessage(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
