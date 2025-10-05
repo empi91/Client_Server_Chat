6d3b50093373a57c79474b28b6213fce61fc014f
empi91 <korpet.filip@gmail.com>
2025-10-05 17:52:15 +0200

    Adding ConnectionPool class, removing old .json db, updating .gitignore

---
+++ connection_pool.py
@@ -0,0 +1,14 @@
+"""Connection Pool module for concurrent connections to database
+
+This module provides a connection pool, which allocated several open connections to database
+at the start, disposes them as needed to required functions and, if necessary, opens and closes additional ones, as per demand
+"""
+
+class ConnectionPool:
+
+    def __init__(self):
+
+        pass
+
+
+    
